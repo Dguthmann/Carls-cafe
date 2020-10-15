dbae9cabdc09112db9eb55063fab78814cec324e
David Guthmann <drguthmann@cox.net>
2020-10-15 11:35:50 -0500

    Make schema and seed objects

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+### Schema
+
+CREATE DATABASE carls_db;
+USE carls_db;
+
+CREATE TABLE burger
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	eaten BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burger (name) VALUES ('Fried Onion Burger');
+INSERT INTO burger (name) VALUES ('Juicy Lucy');
+INSERT INTO burger (name) VALUES ('Loco Moco');
+INSERT INTO burger (name, eaten) VALUES ('Patty Melt', true);
+INSERT INTO burger (name) VALUES ('Runza');
